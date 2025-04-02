8e79c1ae37a73bf831cbffef21e85d3bf352cb13
dannyloja145 <dannyloja145@gmail.com>
2025-04-02 16:33:51 +0000

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'myapp',
+  appId: 'com.humedrexito.app',
+  appName: 'Humedrexito (Off Queroga)',
   webDir: 'dist/myapp/browser'
 };
 
